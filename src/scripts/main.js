03703ccb3a1b68470d2dd16442025d9ad7b63f73
Cecilia C <you@example.com>
2018-01-14 23:23:16 -0500

    fix issue where mobile menu icon background stayed transparent and slightly decreased its size

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,13 +20,13 @@ function showNav() {
 
 function navFlex() {
     $("#overlay").toggle( function(){
-        if ( $(this).toggle(true) ){
+        while ( $(this).toggle(true) ){
             $("#overlay").css("display","flex");
         }
     });
 }
     
- function changeScrollerIcon(){
+function changeScrollerIcon(){
     if ($(window).width() >= 650 ){
         $("#scroll-icon").attr("src","src/assets/icons/001-mouse-clicker.png");
     }
@@ -35,14 +35,16 @@ function navFlex() {
     }
  };
 
- function printCurrentYear () {
+function printCurrentYear () {
     var currentYear = new Date().getFullYear();
     document.getElementById("copy-year").innerHTML = currentYear;  
  }
 
 
- var menu = document.getElementById("mobile-menu");
- document.addEventListener("scroll", function () {
+
+document.addEventListener("scroll", function () {
+    var menu = document.getElementById("mobile-menu");
+    
     if (document.body.scrollTop >= 605) {
         menu.style.background = "#eb8cb6";
         menu.style.boxShadow = "1px 2px 7px #888";
@@ -58,5 +60,4 @@ function navFlex() {
  /*
  * TODO:
     1. Add validation to contact sform (see formspree.io)
-    2. Explore image loading with emergence.js
 */
