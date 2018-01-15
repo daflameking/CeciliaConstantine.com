47fb9f0388649503ad20b945188dcbf0a1f113d1
Cecilia C <you@example.com>
2018-01-15 00:05:31 -0500

    staging for mobile animation and form input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,14 +9,34 @@ $(document).ready(function(){
     printCurrentYear();
 });
 
+
+/*
+ * if mobile menu is clicked toggle overlay
+ * if overlay is toggled add classes animated bounceIn === done through html class
+ * if mobile menu icon is clicked while overlay is display flex aka toggle true add class bounceOut
+ * Algo:
+ * Remove the class from the html and do it all dynamically
+ * use e.preventDefault: https://stackoverflow.com/questions/19927918/toggling-animations-when-using-animate-css
+ * 
+ * 
+*/
 function showNav() {
     $("#mobile-menu").on("click", (function () {
         $("#overlay").toggle();
     }));
+
     $("#overlay a").on("click", function (){
         $("#overlay").toggle(false);
     });
 }
+
+// function closingAnimation () {
+//     if ( $("#overlay").toggle(true) ) {
+//         $("mobile-menu").on("click", function () {
+//             $("#overlay").addClass("bounceOut");
+//         })
+//     }
+// }
 
 function navFlex() {
     $("#overlay").toggle( function(){
@@ -40,8 +60,6 @@ function printCurrentYear () {
     document.getElementById("copy-year").innerHTML = currentYear;  
  }
 
-
-
 document.addEventListener("scroll", function () {
     var menu = document.getElementById("mobile-menu");
 
@@ -50,11 +68,10 @@ document.addEventListener("scroll", function () {
         menu.style.boxShadow = "1px 5px 32px -9px #555";
     }
     else {
-    menu.style.background = "transparent";
-    menu.style.boxShadow = "none";
+        menu.style.background = "transparent";
+        menu.style.boxShadow = "none";
     }
 });
-
 
  /*
  * TODO:
